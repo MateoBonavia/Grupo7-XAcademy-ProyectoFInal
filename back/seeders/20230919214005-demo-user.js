a60470d8f84e6f931e1db18527870210351e4fce
Alessio <lautaroalessio@gmail.com>
2023-09-19 18:47:43 -0300

    Ruteo POST loggin

---
+++ back/seeders/20230919214005-demo-user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      "id": "1",
+      "email": "admin@gmail.com",
+      "password": "admin",
+      "user": "admin"
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
